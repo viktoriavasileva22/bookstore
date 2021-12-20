2e8bc609ee20f90bc599f5736fe3aa0a812fa7dc
viktoriavasileva22 <vikito220899@gmail.com>
2021-12-20 21:13:52 +0200

    Adding BookController

--- src/main/java/com/example/aacademy/bookstore/controller/BookController.java
+++ src/main/java/com/example/aacademy/bookstore/controller/BookController.java
@@ -1,4 +1,57 @@
 package com.example.aacademy.bookstore.controller;
 
+import com.example.aacademy.bookstore.converter.BookConverter;
+import com.example.aacademy.bookstore.dto.BookDto;
+import com.example.aacademy.bookstore.model.Book;
+import com.example.aacademy.bookstore.service.BookService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/books")
+@AllArgsConstructor
 public class BookController {
+
+    private final BookService bookService;
+    private final BookConverter bookConverter;
+
+    @GetMapping
+    public ResponseEntity<Set<BookDto>> findAll(){
+        return ResponseEntity.ok(bookService
+                .findAll()
+                .stream()
+                .map(bookConverter::toBookDto)
+                .collect(Collectors.toSet()));
+    }
+    @GetMapping("/id/{id}")
+    public ResponseEntity<BookDto>findById(@PathVariable Long id){
+        return ResponseEntity.ok(bookConverter.toBookDto(bookService.findById(id)));
+    }
+
+    /*@PutMapping("/{id}")
+    public ResponseEntity<BookDto> update(@RequestBody @Valid BookDto bookDto,
+                                          @PathVariable Long id) {
+        Book book = bookConverter.toBook(bookDto);
+        Book savedBook=bookService.save(book);
+        return ResponseEntity.ok(bookConverter.toBookDto(savedBook));
+    }*/
+
+    @PostMapping
+    public ResponseEntity<BookDto> save(@RequestBody @Valid BookDto bookDto){
+        Book book= bookConverter.toBook(bookDto);
+        Book savedBook=bookService.save(book);
+        return ResponseEntity.ok(bookConverter.toBookDto(savedBook));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable Long id){
+        bookService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
